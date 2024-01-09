4a837310688fb93175ad57d211227358834e6a37
ClerZ <150576610+ClerZ@users.noreply.github.com>
2024-01-09 13:23:20 +0800

    Add files via upload

---
+++ WEB SITE PROJECT/js/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Add smooth scrolling to links with the "scroll-link" class
+  var scrollLinks = document.querySelectorAll('.scroll-link');
+  
+  scrollLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      var targetId = this.getAttribute('href').substring(1);
+      var targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+      }
+    });
+  });
+});
+
+function performSearch() {
+  var searchTerm = document.getElementById('searchInput').value.trim().toLowerCase();
+
+  // Check if the search term is not empty
+  if (searchTerm !== '') {
+    var flavorContainers = document.querySelectorAll('.txtformat');
+
+    // Remove existing highlights
+    var elements = document.querySelectorAll('.highlight');
+    elements.forEach(function (element) {
+      element.classList.remove('highlight');
+    });
+
+    flavorContainers.forEach(function (container) {
+      var flavorText = container.textContent.toLowerCase();
+
+      if (flavorText.includes(searchTerm)) {
+        container.classList.add('highlight');
+
+        container.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+    });
+  }
+}
+
+function toggleCart() {
+  console.log("working");
+  const cartContainer = document.querySelector('.cart-container');
+  const cartContainerStyle = window.getComputedStyle(cartContainer);
+  const isOpen = cartContainerStyle.getPropertyValue('right') === '0px' || cartContainerStyle.getPropertyValue('right') === '';
+
+  if (isOpen) {
+    cartContainer.style.right = '-400px';
+  } else {
+    cartContainer.style.right = '0px';
+  }
+}
